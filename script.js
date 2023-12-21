5e3dddeedd8c61714c814c8ef10adb2003809c93
GITHUB-Vipul <126069867+GITHUB-Vipul@users.noreply.github.com>
2023-12-21 13:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.addEventListener("DOMContentLoaded",()=>
+	{
+		//let ele=document.createElement("p");
+		//ele.innerText="DOM load success";
+		document.body.innerText="DOM load success";
+})
